493401e7bc66a242aae1e61d9afde870b8b958f7
Emily <emelizadaniels@gmail.com>
2022-09-26 15:11:14 -0400

    commit tree 0ca8287b338a96d08a422e1df02dfb580482409f

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement("h1");
+// document.append(newHeader);
+
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Emily is the champion";
